467084c773ecb8f75e85eef39ae20ae0cfca1552
sanjaykaushik29 <sanjay@thoughtwin.com>
2023-08-04 15:53:08 +0530

    new

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const { connection } = require("./db");
+// const { connection } = require("./db");
 
 const app = express();
 
-app.get("/api/shopping-list", (req, res) => {
-  connection.query("SELECT * FROM shopping_list", (err, data) => {
-    if (err) return callback(err, null);
-    res.status(200).json({
-      status: "success",
-      length: data.length,
-      data,
-    });
-  });
-});
+// app.get("/api/shopping-list", (req, res) => {
+//   connection.query("SELECT * FROM shopping_list", (err, data) => {
+//     if (err) return callback(err, null);
+//     res.status(200).json({
+//       status: "success",
+//       length: data.length,
+//       data,
+//     });
+//   });
+// });
 
 
 
